41382c36a028d5734fa25355d6aa3649d882cd22
mickaelyoshua <mickaelyoshua@gmail.com>
2023-04-21 11:08:46 -0300

    creating helper functions to all processes

--- dbc2csv_files/dbc2csv.r
+++ dbc2csv_files/dbc2csv.r
@@ -1,7 +1,5 @@
-install.packages("https://cran.r-project.org/src/contrib/Archive/read.dbc/read.dbc_1.0.5.tar.gz", repos = NULL)
-
 library(read.dbc)
-dbc_file_path <- Sys.getenv("DBC_FILE_PATH") # "./dbc_files/PAPB2201.dbc"
-csv_file_path <- Sys.getenv("CSV_FILE_PATH") # "./csv_files/PAPB2201.csv"
+dbc_file_path <- Sys.getenv("DBC_FILE_PATH")
+csv_file_path <- Sys.getenv("CSV_FILE_PATH")
 x <- read.dbc(dbc_file_path)
 write.csv(x, csv_file_path, row.names = FALSE)
--- helper_functions.py
+++ helper_functions.py
@@ -11,6 +11,7 @@ def print_error() -> None:
     sys.exit()
 
 def get_ftp_files_names(site: str, ftp_folder: str, base_file_name: str) -> list[str]:
+    """Get from ftp connections a list of files names from the given folder starting with the base_file_name"""
     with FTP(site) as ftp:
         try:
             # LOGIN
@@ -35,11 +36,11 @@ def get_ftp_files_names(site: str, ftp_folder: str, base_file_name: str) -> list
         except:
             print_error()
 
-def get_list_names_dbcfiles_bucket(s3_client: boto3.client, bucket: str, prefrix: str) -> list[str]:
+def get_list_files_names_bucket(s3_client: boto3.client, bucket: str, prefrix: str) -> list[str]:
     try:
         print("Getting list of zipfiles in S3 Bucket...")
         try:
-            response = s3_client.list_objects(Bucket=bucket, Prefix="dbcfiles/"+prefrix)["Contents"]
+            response = s3_client.list_objects(Bucket=bucket, Prefix=prefrix)["Contents"]
             content_dbcfiles = [k["Key"] for k in response]
             if len(content_dbcfiles) == 1:
                 print(f"No dbc files in Bucket folder 'dbcfiles/'+{prefrix}.\n")
@@ -53,21 +54,21 @@ def get_list_names_dbcfiles_bucket(s3_client: boto3.client, bucket: str, prefrix
         print("Error getting names of zipfiles in Bucket.")
         print_error()
 
-def upload_dbcfile(s3_resource: boto3.resource, filename: str, bucket: str, key: str, prefrix: str) -> None:
-    """Save local dbcfile on S3 Bucket folder dbcfiles/"""
+def upload_file_to_bucket(s3_resource: boto3.resource, filename: str, bucket: str, prefrix: str) -> None:
+    """Save local file to S3 Bucket on folder inserted on prefix"""
     try:
         print(f"Uploading {filename}...")
         s3_resource.meta.client.upload_file(
             Filename=filename,
             Bucket=bucket,
-            Key="dbcfiles/" + prefrix + key
+            Key=prefrix + filename
         )
         print(f"{filename} uploaded.\n")
     except:
         print("Error uploading files.")
         print_error()
 
-def download_dbcfile(site: str, ftp_folder: str, dbc_files_path: str, file: str) -> None:
+def download_file_from_ftp(site: str, ftp_folder: str, file: str) -> None:
     """Access the ftp connection, go to folder and download files with the base name passed as an argument."""
     with FTP(site) as ftp:
         try:
@@ -86,7 +87,7 @@ def download_dbcfile(site: str, ftp_folder: str, dbc_files_path: str, file: str)
                 sys.exit()
         except:
             print_error()
-        with open(dbc_files_path + f"{file}", "wb") as f:
+        with open("./" + file, "wb") as f:
             print(f"Downloading {file}...")
             retCode = ftp.retrbinary(f"RETR {file}", f.write) # download the file and return a string with the response
             if retCode.startswith("226"):
