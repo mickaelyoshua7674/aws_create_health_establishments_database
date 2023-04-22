86fb8ae6bf66378ff5a91e6735d667c21f521005
mickaelyoshua <mickaelyoshua@gmail.com>
2023-04-22 16:21:21 -0300

    add commands to install docker compose

--- run_container.py
+++ run_container.py
@@ -1,7 +1,11 @@
 install_config_docker = "sudo yum update -y && " + \
     "sudo yum install docker -y && " + \
     "sudo service docker start && " + \
-    "sudo usermod -a -G docker ec2-user"
+    "sudo usermod -a -G docker ec2-user && " + \
+    "yum install curl && " + \
+    "curl -L 'https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose && " + \
+    "chmod +x /usr/local/bin/docker-compose && " + \
+    "ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"
 
 import os
 
