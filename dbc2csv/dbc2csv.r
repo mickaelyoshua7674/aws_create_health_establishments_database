e4ffc57a40ce7f18126f663ffd7d97340c414551
mickaelyoshua <mickaelyoshua@gmail.com>
2023-04-22 16:04:56 -0300

    script for install docker and run container

--- dbc2csv/dbc2csv.r
+++ dbc2csv/dbc2csv.r
@@ -1,6 +1,10 @@
 install.packages('https://cran.r-project.org/src/contrib/Archive/read.dbc/read.dbc_1.0.5.tar.gz', repos = NULL)
 library(read.dbc)
+
 dbc_file_path <- Sys.getenv("DBC_FILE_PATH")
+print(dbc_file_path)
 csv_file_path <- Sys.getenv("CSV_FILE_PATH")
+print(csv_file_path)
+
 x <- read.dbc(dbc_file_path)
 write.csv(x, csv_file_path, row.names = FALSE)
--- run_container.py
+++ run_container.py
@@ -3,9 +3,9 @@ install_config_docker = "sudo yum update -y && " + \
     "sudo service docker start && " + \
     "sudo usermod -a -G docker ec2-user"
 
-import subprocess
 import os
-#p = subprocess.run("ls", shell=True)
-#print("The exit code was: %d" % list_files.returncode)
+
+os.system(install_config_docker)
 
 os.system("DBC_FILE_PATH=dbc_files/PAPB2201.dbc CSV_FILE_PATH=csv_files/PAPB2201.csv docker compose up")
+#os.system("set DBC_FILE_PATH=./dbc2csv/dbc_files/PAPB2201.dbc&& set CSV_FILE_PATH=./dbc2csv/csv_files/PAPB2201.csv&& docker compose up")
