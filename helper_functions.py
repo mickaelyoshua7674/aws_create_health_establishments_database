0108d0ae14be30dd1bfcfbb7012d49bded416c2c
mickaelyoshua <mickaelyoshua@gmail.com>
2023-04-22 12:30:10 -0300

    new script for unzip and organize cnes tables

--- helper_functions.py
+++ helper_functions.py
@@ -2,6 +2,15 @@ from ftplib import FTP
 import traceback
 import sys
 import boto3
+import zipfile
+import io
+import csv
+
+target_tables = ["tbGrupoAtividade", "tbAtividade", "tbGrupoEquipe", "tbTipoEquipe", "tbEquipe",
+                 "tbAtividadeProfissional", "tbConselhoClasse", "tbDadosProfissionalSus", "tbCargaHorariaSus",
+                 "tbConvenio", "tbAtendimentoPrestado", "rlEstabAtendPrestConv", "tbMunicipio",
+                 "tbLeito", "rlEstabComplementar", "tbNaturezaJuridica", "tbTipoUnidade",
+                 "tbTipoEstabelecimento", "tbEstabelecimento"]
 
 def print_error() -> None:
     """Print the error message and exit script."""
@@ -38,21 +47,21 @@ def get_files_names_ftp(site: str, ftp_folder: str, base_file_name: str) -> list
 def get_files_names_bucket(s3_client: boto3.client, bucket: str, prefrix: str) -> list[str]:
     """Get list of all files names if Se Bucket folder. If folders doesn't exist or is empty return an empty list."""
     try:
-        print("Getting list of files in S3 Bucket...")
+        print(f"Getting list of files in S3 Bucket folder '{prefrix}'...")
         try:
             response = s3_client.list_objects(Bucket=bucket, Prefix=prefrix)["Contents"]
             content_dbcfiles = [k["Key"] for k in response]
             if len(content_dbcfiles) == 1:
-                print(f"No dbc files in Bucket folder {prefrix}.\n")
+                print(f"No files in Bucket folder '{prefrix}'.\n")
                 return []
             else:
                 print("Names collected.\n")
                 return [item[len(prefrix):] for item in content_dbcfiles]
         except KeyError: # if folder doesn't exist will be no 'Contents' key so is returned a KeyError
-            print("Folder doesn't exist in Bucket.")
+            print(f"'{prefrix}' doesn't exist in Bucket.\n")
             return []
     except:
-        print("Error getting names of files in Bucket.")
+        print(f"Error getting names of files in Bucket folder '{prefrix}'.")
         print_error()
 
 def upload_file_to_bucket(s3_resource: boto3.resource, filename: str, bucket: str, prefrix: str) -> None:
@@ -95,3 +104,32 @@ def download_file_from_ftp(site: str, ftp_folder: str, file: str) -> None:
                 print(f"{file} downloaded.\n")
             else:
                 print(f"Error downloading {file}: {retCode}")
+
+def download_file_bucket(s3_client: boto3.client, bucket: str, file: str, prefix: str) -> None:
+    """Download file from S3 Bucket in the same folder that the script is located."""
+    print(f"Downloading {file}...")
+    s3_client.download_file(
+        Bucket=bucket,
+        Key=prefix + file,
+        Filename="./" + file
+    )
+    print(f"{file} downloaded.\n")
+
+def unzip_and_organize(s3_client: boto3.client, bucket: str, zip: str, prefix: str, tables_uploaded: list[str]) -> None:
+    """"""
+    print(f"\nOpening {zip}...")
+    with zipfile.ZipFile(zip, "r") as zf: # openning zipfile
+        for table_name in zf.namelist():
+            base_table_name = table_name.split(".")[0][:-6]
+
+            if base_table_name in target_tables and base_table_name not in tables_uploaded:
+                print(f"Writing {table_name} into S3 Bucket...")
+                with zf.open(table_name, "r") as table: # opening target file
+                    r = csv.reader(io.TextIOWrapper(table, "utf-8"), delimiter=";") # decoding and reading file
+                    rows = [row for row in r]
+
+                    buff = io.StringIO()
+                    csv.writer(buff).writerows(rows)
+
+                    s3_client.put_object(Bucket=bucket, Key=prefix + table_name, Body=buff.getvalue().encode("utf-8", "replace"))
+                    print(f"{table_name} written.\n")
--- unzip_and_organize.py
+++ unzip_and_organize.py
@@ -1,107 +1,29 @@
-import io
-import csv
-import zipfile
-import traceback
-import sys
+from helper_functions import *
 import os
-import boto3
-import shutil
 
-BUCKET_NAME = "files-cnes-datasus"
+BUCKET_NAME = "laos-datasus"
+SITE = "ftp.datasus.gov.br"
+
+BUCKET_FOLDER_ZIPFILES = "cnes/zipfiles/"
+
+BUCKET_FOLDER_RAW_TABLES = "cnes/raw_tables/"
 
 s3_client = boto3.client("s3")
 s3_resource = boto3.resource("s3")
-#-----------------------------------------------------------------FUNCTIONS-----------------------------------------------------------------#
-def print_error() -> None:
-    """Print the error message and exit script."""
-    traceback.print_exc()
-    print("Closing...")
-    sys.exit()
 
-def get_list_names_zipfiles_bucket(s3_client: boto3.client, bucket: str) -> list[str]:
-    try:
-        print("Getting list of zipfiles in S3 Bucket...")
-        try:
-            response = s3_client.list_objects(Bucket=bucket, Prefix="zipfiles/")["Contents"]
-            content_zipfiles = [k["Key"] for k in response]
-            if len(content_zipfiles) == 1:
-                print("No zip files in Bucket folder 'zipfiles/'.\n")
-                return []
-            else:
-                print("Names collected.\n")
-                return [item[len("zipfiles/"):] for item in content_zipfiles][1:]
-        except KeyError: # if folder doesn't exist will be no 'Contents' key so is returned a KeyError
-            return []
-    except:
-        print("Error getting names of zipfiles in Bucket.")
-        print_error()
+# GET ZIPFILES NAMES FROM S3 BUCKET
+zipfiles_names_bucket = get_files_names_bucket(s3_client, BUCKET_NAME, BUCKET_FOLDER_ZIPFILES)
 
-def download_zipfile_bucket(s3_client: boto3.client, bucket: str, file: str) -> None:
-    """Download zipfile from S3 Bucket in the same folder that the script is located."""
-    print(f"Downloading {file}...")
-    s3_client.download_file(
-        Bucket=bucket,
-        Key="zipfiles/" + file,
-        Filename="./" + file
-    )
-    print(f"{file} downloaded.\n")
-
-def unzip_and_organize(s3_client: boto3.client, bucket: str, zip: str, folder: str) -> None:
-    """"""
-    print(f"\nUnziping {zip}...")
-    with zipfile.ZipFile(zip, "r") as zf: # openning zipfile
-        print(f"{zip} unziped.\n")
-        for f in zf.namelist():
-            print(f"Writing {f}...")
-            with zf.open(f, "r") as table: # opening target file
-                r = csv.reader(io.TextIOWrapper(table, "utf-8"), delimiter=";") # decoding and reading file
-                rows = [row for row in r]
-
-                buff = io.StringIO()
-                csv.writer(buff).writerows(rows)
-
-                s3_client.put_object(Bucket=bucket, Key="raw_tables/" + folder + f, Body=buff.getvalue().encode("utf-8", "replace"))
-                print(f"{f} written.")
-
-def get_list_names_raw_tables_bucket(s3_client: boto3.client, bucket: str, folder: str) -> list[str]:
-    """Get list of all content in 'raw_tables/' plus the folder from the input"""
-    try:
-        response = s3_client.list_objects(Bucket=bucket, Prefix="raw_tables/"+folder)["Contents"]
-        return [k["Key"] for k in response if k["Key"]]
-    except KeyError: # if folder doesn't exist will be no 'Contents' key so is returned a KeyError
-        return []
-
-def upload_tables(s3_resource: boto3.resource, bucket: str, z: str, folder) -> None:
-    print(f"Extracting {z}...")
-    with zipfile.ZipFile(z, "r") as zf: # openning zipfile
-        zf.extractall(folder)
-        print(f"{z} extracted.\n")
-    os.remove(z)
-
-    print(f"Uploading tables from {z}...")
-    for table in os.listdir(folder):
-        s3_resource.meta.client.upload_file(
-            Filename=folder + table,
-            Bucket=bucket,
-            Key="raw_tables/" + folder + table
-        )
-    print("Tables uploaded.\n")
-    shutil.rmtree(folder[:4])
-#-----------------------------------------------------------------SCRIPT-----------------------------------------------------------------#
-count = 0
-# GET ZIPFILES NAMES FROM 'zipfiles/'
-names_zipfiles_bucket = get_list_names_zipfiles_bucket(s3_client, BUCKET_NAME)
-# DOWNLOAD ZIPFILES, UNZIP AND WRTIE CSV INTO 'raw_tables/'
-for z in names_zipfiles_bucket:
+for z in zipfiles_names_bucket:
     year_month = z.split(".")[0][-6:]
-    folder = year_month[:4] + "/" + year_month[-2:] + "/"
-    names_raw_tables = get_list_names_raw_tables_bucket(s3_client, BUCKET_NAME, folder)
-    if len(names_raw_tables) == 0:
-        download_zipfile_bucket(s3_client, BUCKET_NAME, z)
-        os.makedirs(folder)
-        upload_tables(s3_resource, BUCKET_NAME, z, folder)
-    else:
-        count += 1
+    folder = year_month[:4] + "/" + year_month[-2:] + "/" # Getting path to organized tables
+    raw_tables_names_bucket = get_files_names_bucket(s3_client, BUCKET_NAME, BUCKET_FOLDER_RAW_TABLES + folder)
 
-if len(names_zipfiles_bucket) == count:
-    print("All zip files extracted and tables organized.")
+    base_name_raw_tables_names_bucket = [t.split(".")[0][:-6] for t in raw_tables_names_bucket]
+
+    if sorted(base_name_raw_tables_names_bucket) == sorted(target_tables): # if all target files are already on Bucket folder
+        print(f"All files in '{BUCKET_FOLDER_RAW_TABLES + folder}' are updated.")
+    else:
+        download_file_bucket(s3_client, BUCKET_NAME, z, BUCKET_FOLDER_ZIPFILES)
+        unzip_and_organize(s3_client, BUCKET_NAME, z, BUCKET_FOLDER_RAW_TABLES + folder, base_name_raw_tables_names_bucket)
+        os.remove(z)
