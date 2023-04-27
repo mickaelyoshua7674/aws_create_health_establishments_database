86f98279c7e1fb3cdd07ead5a1b03ff39a4ce0e2
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-04-27 11:50:13 -0300

    r

--- dbc2csv/config_ec2_instance.py
+++ dbc2csv/config_ec2_instance.py
@@ -1,15 +1,17 @@
 script = \
     "sudo yum update -y && " + \
     "sudo yum install git -y && " + \
-    "sudo yum install docker -y && " + \
-    "sudo usermod -a -G docker ec2-user && " + \
-    "sudo service docker start && " + \
-    "sudo chkconfig docker on && " + \
-    "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && " + \
-    "sudo chmod +x /usr/local/bin/docker-compose && " + \
+    "sudo yum install R -y && " + \
     "sudo yum install python3-pip -y && " + \
     "pip install boto3 && " + \
-    "git clone https://github.com/mickaelyoshua7674/aws_create_health_establishments_database.git && " + \
-    "newgrp docker"
+    "git clone https://github.com/mickaelyoshua7674/aws_create_health_establishments_database.git"
+"""
+sudo yum update -y
+sudo yum install git -y
+sudo yum install R -y
+sudo yum install python3-pip -y
+pip install boto3
+git clone https://github.com/mickaelyoshua7674/aws_create_health_establishments_database.git
+"""
 
 # config awscli
