d4acaa7f2a48a1c13423caa3cfd4a5c99055b092
mickaelyoshua <mickaelyoshua@gmail.com>
2023-04-27 15:49:11 -0300

    r

--- dbc2csv/dbc2csv.r
+++ dbc2csv/dbc2csv.r
@@ -1,9 +1,14 @@
 # ‘~/R/x86_64-amazon-linux-gnu-library/4.1’
+# 'C:\Users\micka\AppData\Local/R/win-library/4.3'
 # install.packages("languageserver")
+# install.packages("devtools")
+# devtools::install_url("https://cran.r-project.org/src/contrib/Archive/read.dbc/read.dbc_1.0.5.tar.gz")
 # install.packages("https://cran.r-project.org/src/contrib/Archive/read.dbc/read.dbc_1.0.5.tar.gz", repos = NULL)
+
+
 library(read.dbc)
 
-dbc_file_path <- Sys.getenv("DBC_FILE_PATH") # DBC_FILE_PATH=PAPB2209.dbc CSV_FILE_PATH=PAPB2209.csv Rscript dbc2csv.r
+dbc_file_path <- Sys.getenv("DBC_FILE_PATH")
 print(dbc_file_path)
 csv_file_path <- Sys.getenv("CSV_FILE_PATH")
 print(csv_file_path)
