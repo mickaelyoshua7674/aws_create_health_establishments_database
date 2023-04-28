855c23ad7b00912a23cff4b6d62c1119e86e2732
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-04-28 09:15:12 -0300

    trying in ubuntu instance

--- dbc2csv/config_ec2_instance.py
+++ dbc2csv/config_ec2_instance.py
@@ -14,4 +14,14 @@ pip install boto3
 git clone https://github.com/mickaelyoshua7674/aws_create_health_establishments_database.git
 """
 
+"""
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install r-base -y
+sudo apt-get install python3-pip -y
+sudo apt-get install awscli -y
+pip install boto3
+pip install awscli
+"""
+
 # config awscli
